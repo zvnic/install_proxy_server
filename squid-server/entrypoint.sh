b885ea2e6f8be68a5e079807a36f51f036f9ec9d
zvnic <zvnic@mail.ru>
2025-05-19 01:11:53 +0300

    update entrypoint squid

--- squid-server/entrypoint.sh
+++ squid-server/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Устанавливаем apache2-utils для получения htpasswd
+# Очищаем кэш apt
+rm -rf /var/lib/apt/lists/*
+# Обновляем ключи GPG для репозиториев Ubuntu
+apt-get update -o Acquire::AllowInsecureRepositories=true || true
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32 871920D1991BC93C
+# Обновляем список пакетов и устанавливаем apache2-utils
 apt-get update && apt-get install -y apache2-utils
+# Очищаем кэш apt
+apt-get clean && rm -rf /var/lib/apt/lists/*
 # Создаём файл паролей для Squid
 htpasswd -bc /etc/squid/passwd $HTTP_USER $HTTP_PASS
 # Запускаем Squid
