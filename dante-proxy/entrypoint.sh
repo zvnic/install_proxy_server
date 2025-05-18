33ee49dddd54d3bfaf9e92d99cfa03388dede742
zvnic <zvnic@mail.ru>
2025-05-18 23:34:09 +0300

    feat: обновлена конфигурация Dante proxy

--- dante-proxy/entrypoint.sh
+++ dante-proxy/entrypoint.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
-set -e
 
-envsubst < /etc/danted.conf.template > /etc/danted.conf
+# Создаем пользователя
+adduser -D -H -s /sbin/nologin ${USER:-proxyuser}
+echo "${USER:-proxyuser}:${PASS:-proxypass}" | chpasswd
 
-# Добавление пользователя
-adduser -D "$PROXY_USER"
-echo "$PROXY_USER:$PROXY_PASS" | chpasswd
+# Генерируем конфигурацию
+cat > /etc/danted.conf << EOF
+logoutput: stderr
+internal: 0.0.0.0 port = ${SOCKS_PORT:-1080}
+external: eth0
+socksmethod: username
+user.privileged: root
+user.notprivileged: nobody
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    log: error connect disconnect
+}
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    command: bind connect udpassociate
+    log: error connect disconnect
+    socksmethod: username
+}
+EOF
 
+# Запускаем Dante
 exec danted -f /etc/danted.conf
